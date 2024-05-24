2c6eb4265161a8af1def52924ce1c5642314e1a1
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-05-24 16:01:52 -0300

    system tarefas

---
+++ SistemaDeTarefas/Data/Map/UsuarioMap.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using SistemaDeTarefas.Models;
+
+namespace SistemaDeTarefas.Data.Map
+{
+    public class UsuarioMap : IEntityTypeConfiguration<UsuarioModel>
+    {
+        public void Configure(EntityTypeBuilder<UsuarioModel> builder)
+        {
+            builder.ToTable("Usuarios");
+            builder.HasKey(x => x.Id);
+            builder.Property(x => x.Nome).IsRequired().HasMaxLength(100);
+            builder.Property(x => x.Email).IsRequired().HasMaxLength(100);
+        }
+    }
+}
---
+++ SistemaDeTarefas/Repositorios/UsuarioRepositorio.cs
@@ -0,0 +1,62 @@
+﻿using SistemaDeTarefas.Data;
+using SistemaDeTarefas.Models;
+using SistemaDeTarefas.Repositorios.Interfaces;
+using Microsoft.EntityFrameworkCore;
+using System;
+
+namespace SistemaDeTarefas.Repositorios
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly SistemaDeTarefasDbContext _context;
+
+        public UsuarioRepositorio(SistemaDeTarefasDbContext context)
+        {
+            _context = context;
+
+        }
+        public async Task<UsuarioModel> BuscarUsuarioPorId(int id)
+        {
+            return await _context.Usuarios.FirstOrDefaultAsync(u => u.Id == id);
+        }
+
+        public async Task<List<UsuarioModel>> BuscarUsuarios()
+        {
+            return await _context.Usuarios.ToListAsync();
+        }
+        public async Task<UsuarioModel> Adicionar(UsuarioModel usuario)
+        {
+            await _context.Usuarios.AddAsync(usuario);
+            _context.SaveChanges();
+            return usuario;
+        }
+
+        public async Task<UsuarioModel> Atualizar(UsuarioModel usuario, int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarUsuarioPorId(id);
+            if (usuarioPorId == null)
+            {
+                throw new Exception("Erro ao atualizar o usuário");
+            }
+
+            usuarioPorId.Nome = usuario.Nome;
+            usuarioPorId.Email = usuario.Email;
+            _context.Usuarios.Update(usuarioPorId);
+            await _context.SaveChangesAsync();
+            return usuarioPorId;
+        }
+
+        public async Task<bool> Deletar(int id)
+        {
+            UsuarioModel usuarioPorId = await BuscarUsuarioPorId(id);
+            if (usuarioPorId == null)
+            {
+                throw new Exception("Erro ao deletar o usuário");
+            }
+
+            _context.Usuarios.Remove(usuarioPorId);
+            await _context.SaveChangesAsync();
+            return true;
+        }
+    }
+}
