9916e2daff51a55e4ed0c4a9c8b134c2ef52d2d8
Alisson Rodrigo <alissorodrigo098@gmail.com>
2024-06-03 17:24:21 -0300

    a

--- SistemaDeTarefas/obj/Debug/net8.0/SistemaDeTarefas.AssemblyInfo.cs
+++ SistemaDeTarefas/obj/Debug/net8.0/SistemaDeTarefas.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7cfb2dcdcc872aac3e9ea42a617b7b3970eeabaf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7fd704cc74748693ba8124c2ad76f38578439fa0")]
 [assembly: System.Reflection.AssemblyProductAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SistemaDeTarefas")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
